de44c8f742828046b9fee6707bc9be6a3e5aec4a
AFREENEKRAM <ekramafreen@yahoo.com>
2020-09-13 20:58:48 +0500

    loginForm and highscoreList

--- VirtualLabProjectAngular/src/app/Modules/first-page/first-page.module.ts
+++ VirtualLabProjectAngular/src/app/Modules/first-page/first-page.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 import { HighScoreComponent } from './high-score/high-score.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { FirstScreenComponent } from './first-screen/first-screen.component';
+import { DxDataGridModule } from 'devextreme-angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [HighScoreComponent, LoginFormComponent, FirstScreenComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    DxDataGridModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class FirstPageModule { }
--- VirtualLabProjectAngular/src/app/Modules/first-page/first-screen/first-screen.component.ts
+++ VirtualLabProjectAngular/src/app/Modules/first-page/first-screen/first-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-first-screen',
@@ -7,9 +8,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstScreenComponent implements OnInit {
 
-  constructor() { }
+  loginForm;
+  loginFormFields = new loginFormFields;
+  highScoreList: highscore[];
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+
+    this.initializeDataSet();
+
+    this.highScoreList = [
+      {
+       "Name": "JOHN",
+       "Score": 1200
+      },
+      {
+       "Name": "Mary",
+       "Score": 1100
+      },
+      {
+        "Name": "CIA",
+        "Score": 1000
+       },
+       {
+        "Name": "EZA",
+        "Score": 950
+       },]
+  }
+
+
+  initializeDataSet = () => {
+    this.loginForm = this.formBuilder.group({
+      email: [this.loginFormFields.email || ''],
+      password: [this.loginFormFields.password || ''],
+    })
+  }
+
+
+  onSubmit(){
+
   }
 
 }
+
+export class highscore {
+  Name: string;
+  Score: number;
+}
+
+export class loginFormFields {
+  email: string;
+  password: number;
+}
+
